52573a5892fbfc15e69b52f8ab8fbf8c466cdc72
arthur <arthur.haffemayer@yahoo.fr>
2024-10-01 18:22:48 +0200

    main ocp creation

---
+++ traj-generation/main_ocp.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pinocchio as pin
+
+from wrapper_panda import PandaWrapper
+from visualizer import create_viewer, add_sphere_to_viewer, add_cube_to_viewer
+from param_parsers import ParamParser
+
+import create_ocp
+
+# Creating the robot
+robot_wrapper = PandaWrapper(capsule=True)
+rmodel, cmodel, vmodel = robot_wrapper()
+
+pp = ParamParser("scenes.yaml", 0)
+
+cmodel = pp.add_collisions(rmodel, cmodel)
+
+cdata = cmodel.createData()
+rdata = rmodel.createData()
+
+vis = create_viewer(
+    rmodel, cmodel, cmodel
+)
+
+# Generating the meshcat visualizer
+vis = create_viewer(
+    rmodel, cmodel, cmodel
+)
+add_sphere_to_viewer(vis, "goal", 5e-2,  pp.get_target_pose().translation, color=0x006400)
+OCP_dist = create_ocp.create_ocp_distance(rmodel, cmodel, pp)
+OCP_dist.solve()
+
+
+
+# for i, xs in enumerate(ocp_vel.xs):
+#         q = np.array(xs[:7].tolist())
+#         pin.framesForwardKinematics(rmodel, rdata, q)
+#         add_cube_to_viewer(
+#             vis,
+#             "vcolmpc" + str(i),
+#             [2e-2, 2e-2, 2e-2],
+#             rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
+#             color=100000000,
+#         )
+
+for i, xs in enumerate(OCP_dist.xs):
+        q = np.array(xs[:7].tolist())
+        pin.framesForwardKinematics(rmodel, rdata, q)
+        add_sphere_to_viewer(
+            vis,
+            "colmpc" + str(i),
+            2e-2,
+            rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
+            color=100000,
+        )
