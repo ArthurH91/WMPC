d2d834c8ee294ad82326b2aa271ef437a0d76e7a
arthur <arthur.haffemayer@yahoo.fr>
2024-10-01 18:48:07 +0200

    displaying the two traj

--- traj-generation/main_ocp.py
+++ traj-generation/main_ocp.py
@@ -27,21 +27,26 @@ vis = create_viewer(
     rmodel, cmodel, cmodel
 )
 add_sphere_to_viewer(vis, "goal", 5e-2,  pp.get_target_pose().translation, color=0x006400)
+
+# OCP with distance constraints
 OCP_dist = create_ocp.create_ocp_distance(rmodel, cmodel, pp)
 OCP_dist.solve()
+print("OCP with distance constraints solved")
 
-
-
-# for i, xs in enumerate(ocp_vel.xs):
-#         q = np.array(xs[:7].tolist())
-#         pin.framesForwardKinematics(rmodel, rdata, q)
-#         add_cube_to_viewer(
-#             vis,
-#             "vcolmpc" + str(i),
-#             [2e-2, 2e-2, 2e-2],
-#             rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
-#             color=100000000,
-#         )
+# OCP with velocity constraints
+ocp_vel = create_ocp.create_ocp_velocity(rmodel, cmodel, pp)
+ocp_vel.solve()
+print("OCP with velocity constraints solved")
+for i, xs in enumerate(ocp_vel.xs):
+        q = np.array(xs[:7].tolist())
+        pin.framesForwardKinematics(rmodel, rdata, q)
+        add_cube_to_viewer(
+            vis,
+            "vcolmpc" + str(i),
+            [2e-2, 2e-2, 2e-2],
+            rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
+            color=100000000,
+        )
 
 for i, xs in enumerate(OCP_dist.xs):
         q = np.array(xs[:7].tolist())
@@ -53,3 +58,12 @@ for i, xs in enumerate(OCP_dist.xs):
             rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
             color=100000,
         )
+while True:
+    print("OCP with distance constraints")
+    for q in OCP_dist.xs:
+        vis.display(q[:7])
+        input()
+    print("OCP with velocity constraints")
+    for q in ocp_vel.xs:
+        vis.display(q[:7])
+        input()
